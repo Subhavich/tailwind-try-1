d9428703c05e3a329c3ed89649d734f14ece3a46
Subhavich <subhavichads@gmail.com>
2024-08-10 17:15:48 +0700

    style carousel card; add+style CarouselCardLight

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -1,33 +1,27 @@
 import Slide from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import CarouselCard from "./CarouselCard";
+import CarouselCardLight from "./CarouselCardLight";
 export default function Slider() {
   var settings = {
     dots: true,
     infinite: true,
+    autoplay: true,
     speed: 500,
+    autoplaySpeed: 2000,
     slidesToShow: 1,
     slidesToScroll: 1,
   };
   return (
-    <div className="mt-8">
+    <div className="mt-8  ">
       <Slide {...settings}>
-        <div className="w-full min-h-60 bg-slate-700"></div>
-        <div>
-          <h3>2</h3>
-        </div>
-        <div>
-          <h3>3</h3>
-        </div>
-        <div>
-          <h3>4</h3>
-        </div>
-        <div>
-          <h3>5</h3>
-        </div>
-        <div>
-          <h3>6</h3>
-        </div>
+        <CarouselCard />
+        <CarouselCardLight />
+        <CarouselCard />
+        <CarouselCard />
+        <CarouselCard />
+        <CarouselCard />
       </Slide>
     </div>
   );
